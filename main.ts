5eec2ca79500532b71586694f6e36cc98ab10741
BadIce48 <116098979+BadIce48@users.noreply.github.com>
2022-11-01 11:29:25 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,33 @@
 input.onButtonPressed(Button.A, function () {
-    etat_actuel = 0
+    alarme = false
 })
-let etat_actuel = 0
+let alarme = false
 let nombre = 0
-etat_actuel = 0
+let etat_actuel = 0
 let etat_passe = 0
 basic.forever(function () {
     etat_actuel = pins.digitalReadPin(DigitalPin.P16)
     if (etat_actuel != etat_passe && etat_actuel == 1) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . . . .
-            . . # . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+        alarme = false
     } else {
-    	
+        alarme = true
+    }
+    if (alarme == true) {
+        while (alarme == true) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
     }
 })
